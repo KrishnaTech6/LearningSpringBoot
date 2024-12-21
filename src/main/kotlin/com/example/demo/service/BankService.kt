eef04f0babc7c86fa44803d8baf790ffa5f17a54
krishnaTech6 <krishna060702@gmail.com>
2024-12-21 19:35:59 +0530

    added test for mockk

--- src/main/kotlin/com/example/demo/service/BankService.kt
+++ src/main/kotlin/com/example/demo/service/BankService.kt
@@ -1,11 +1,13 @@
 package com.example.demo.service
 
+import com.example.demo.datasource.BankDataSource
+import com.example.demo.model.Bank
 import org.springframework.stereotype.Service
 
 @Service
 /**The @Service annotation in Spring Boot is used to define a service layer component in your application.
  * It is a specialization of the @Component annotation and
  * is typically used for business logic or service-related functionality.*/
-class BankService {
-
+class BankService(private val dataSource: BankDataSource) {
+    fun getBanks(): Collection<Bank> = dataSource.retrieveBanks()
 }
